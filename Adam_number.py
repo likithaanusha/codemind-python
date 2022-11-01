412efc55f90f8f6187d75cf3ac360f0f0ebf2126
likithaanusha <84706024+likithaanusha@users.noreply.github.com>
2022-11-01 14:54:20 +0530

    n=int(input()) a=str(n) b=list(a) d=n**2    #144 s=a[::-1]   #2 1 g=int(s)    #21 f=g**2 #441 i=str(f)  #4 4 1 h=i[::-1]    #1 4 4 j=int(h)     #144 if (d==j):     print("True") else:     print("False")

---
+++ Adam_number.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=str(n)
+b=list(a)  
+d=n**2    #144
+s=a[::-1]   #2 1
+g=int(s)    #21
+f=g**2 #441
+i=str(f)  #4 4 1
+h=i[::-1]    #1 4 4
+j=int(h)     #144
+if (d==j):
+    print("True")
+else:
+    print("False")
+
